de0734035e2995d9473478209366dbeb0834107d
Kelvin Roman Domenech <KRomanDomenech@student.fullsail.edu>
2022-06-16 18:47:51 -0500

    Finish the Midterm with a simple solution, will be refactoring

--- Midterm_Practical/Midterm_Practical/Midterm_Practical.cpp
+++ Midterm_Practical/Midterm_Practical/Midterm_Practical.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
-void DisplayRandomQuestion(const char* questionList[], int numOfQuestions, const char* answerList[][4]);
+int DisplayRandomQuestion(const char* questionList[], int numOfQuestions, const char* answerList[][4]);
+int ReadAndValidateInput(const char* message, int minSelection, int maxSelection);
+void resetConsoleBuffer();
+bool isCorrect(int questionsIndex, int userSelection);
 
 int main()
 {
@@ -14,12 +17,35 @@ int main()
 								 {"1. Leonardo DiCaprio", "2. Giovanna Garzoni", "3. Pablo Picasso", "4. Leonardo Da Vinci"},
 								 {"1.   5", "2.   4", "3.   2", "4.   3"},
 								 {"1. Oasis", "2. Periapsis", "3. Apoapsis", "4. Perigee"} };
+	bool keepGoing = true;
+	while (keepGoing)
+	{
+		int questionIndex = DisplayRandomQuestion(questions, 4, answers);
+		int userSelection = ReadAndValidateInput("Choose an answer 1-4: ", 1, 4);
+		bool isUserCorrect = isCorrect(questionIndex, userSelection);
 
-	DisplayRandomQuestion(questions, 4, answers);
+		if (isUserCorrect)
+			std::cout << "Good job! You got the answer right.";
+		else
+			std::cout << "I am sorry but that is not the correct answer.";
+
+		int continueOrNot = ReadAndValidateInput(" Press 1 to continue, Press 2 to quit: ", 1, 2);
+		if (continueOrNot == 1)
+		{
+			continue;
+		}
+		else
+		{
+			break;
+		}
+	}
+	
+	std::cout << "Thank you please come again! \n";
 	system("pause");
 }
 
-void DisplayRandomQuestion(const char* questionList[], int numOfQuestions, const char* answerList[][4])
+//will return a number to be able to see later what question was used.
+int DisplayRandomQuestion(const char* questionList[], int numOfQuestions, const char* answerList[][4])
 {
 
 	//this will getting the number of questions from the array. dividing by size of void* because it is dependent on if running in 64 or 32 bit.
@@ -29,4 +55,49 @@ void DisplayRandomQuestion(const char* questionList[], int numOfQuestions, const
 	{
 		std::cout << answerList[randomIndex][i] << std::endl;
 	}
+
+	return randomIndex;
+}
+
+int ReadAndValidateInput(const char* message, int minSelection, int maxSelection)
+{
+	int answerSelection = 0;
+
+	while (true)
+	{
+		std::cout << message;
+		std::cin >> answerSelection;
+		if (std::cin.fail() || (answerSelection < minSelection || answerSelection > maxSelection))
+		{
+			resetConsoleBuffer();
+			std::cout << "\nYour input was not valid" << std::endl;
+		}
+			
+		else
+			break;
+	}
+
+	resetConsoleBuffer();
+	return answerSelection;
+}
+
+bool isCorrect(int questionsIndex, int userSelection)
+{
+	bool correctOrNot = false;
+	//all the condition in which the user is right
+	if ((questionsIndex == 0 && userSelection == 2) ||
+		(questionsIndex == 1 && userSelection == 4) ||
+		(questionsIndex == 2 && userSelection == 1) ||
+		(questionsIndex == 3 && userSelection == 3))
+	{
+		correctOrNot = true;
+	}
+
+	return correctOrNot;
+
+}
+void resetConsoleBuffer()
+{
+	std::cin.clear();
+	std::cin.ignore(INT_MAX, '\n');
 }
